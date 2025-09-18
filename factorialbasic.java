ac9dbba3442cb66b91ca4873f8d9053db73f8de6
Amitoj Singh Gambhir <gambhir.amitoj@gmail.com>
2025-09-18 16:01:27 +0530

    Update factorialbasic.java

--- factorialbasic.java
+++ factorialbasic.java
@@ -1,2 +1,20 @@
 // basic factorial recursion program in java. 
 
+import java.util.Scanner;
+
+public class Factorialbasic {
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number of which you want to find factorial.");
+		int number= sc.nextInt();
+		
+	long fact=1;	
+	
+	for(int i=1; i<=number; i++) {
+		fact=fact*i;
+		System.out.println(fact);
+		
+	}
+}
+}
